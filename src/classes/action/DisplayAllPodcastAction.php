a5582b68b7b851174a4ffec35de59b1de502fee7
Nathan Eyer <nathan.eyer52700@gmail.com>
2024-11-06 12:27:08 +0100

    Ajout d'une nouvelle action

---
+++ src/classes/action/DisplayAllPodcastAction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace iutnc\deefy\action;
+
+use iutnc\deefy\action\Action;
+use iutnc\deefy\render\AlbumTrackRenderer;
+use iutnc\deefy\render\PodcastRenderer;
+use iutnc\deefy\repository\DeefyRepository;
+
+class DisplayAllPodcastAction extends Action
+{
+
+    public function execute(): string
+    {
+        try{
+
+            $r = DeefyRepository::getInstance() ;
+
+            $a = $r->findAllPodcast() ;
+            $ret = "" ;
+
+            foreach ($a as $track) {
+                $renderer = new PodcastRenderer($track) ;
+                $ret .= $renderer->render(1) ;
+                $ret .= "<br>" ;
+            }
+            return $ret ;
+        } catch(\Exception $e) {
+            return "error" ;
+        }
+    }
+}
